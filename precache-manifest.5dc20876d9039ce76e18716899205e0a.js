7f690350b433fa7138ffdcbbebb97ceb054c42f4
Kornél (via Travis CI) <stefankornel02@gmail.com>
2020-05-27 19:20:52 +0000

    Deploy hazizz.hu/vakacio/ to github.com/hazizz/vakacio.git:gh-pages

---
+++ precache-manifest.5dc20876d9039ce76e18716899205e0a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a646b03c7d8794768a88ca1b688ffef",
+    "url": "https://hazizz.hu/vakacio/index.html"
+  },
+  {
+    "revision": "526fdc249acd29c580c1",
+    "url": "https://hazizz.hu/vakacio/static/css/main.9087c2cb.chunk.css"
+  },
+  {
+    "revision": "2f4b394a25cafa6f0d8c",
+    "url": "https://hazizz.hu/vakacio/static/js/2.2b521f5b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "https://hazizz.hu/vakacio/static/js/2.2b521f5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "526fdc249acd29c580c1",
+    "url": "https://hazizz.hu/vakacio/static/js/main.08ec371e.chunk.js"
+  },
+  {
+    "revision": "1cc944f30895a4e8a1ac",
+    "url": "https://hazizz.hu/vakacio/static/js/runtime-main.ad3fad64.js"
+  },
+  {
+    "revision": "405f23af4bcc00c9716de5bae1f1179f",
+    "url": "https://hazizz.hu/vakacio/static/media/ChelseaMarket-Regular.405f23af.ttf"
+  },
+  {
+    "revision": "9406d0ab606cf8cb91c41b65556bd836",
+    "url": "https://hazizz.hu/vakacio/static/media/Lobster-Regular.9406d0ab.ttf"
+  },
+  {
+    "revision": "1c03ed6d21f1f2549254f51c905e0104",
+    "url": "https://hazizz.hu/vakacio/static/media/chawp.regular.1c03ed6d.ttf"
+  }
+]);
